5b65016d337fff29cc5fae55b0856d57ff55f342
Dmitry <dmitry1298@yandex.ru>
2024-08-25 11:02:58 +1000

    Add converter for urls

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -5,5 +5,13 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse('Страница')
 
-def categories(request):
-    return HttpResponse('<h1>Страница по категориям</h1>')
+def categories(request, cat_id):
+    return HttpResponse(f'<h1>Страница по категориям</h1><p>id: {cat_id} </p>') 
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1>Страница по категориям</h1><p>slug: {cat_slug} </p>') 
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year} </p>') 
+   
