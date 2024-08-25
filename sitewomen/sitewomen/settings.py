80f6f4a3f7a7669dd4d50ed254cfd43957575cd4
Dmitry <dmitry1298@yandex.ru>
2024-08-25 14:18:30 +1000

    Add redirect and handler debug page

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$^k9wptg0xg0ji7!+hf&xl7o)5zyoblfau#0gx=yg5xd_z(5ih'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 
 def index(request):
-    return HttpResponse('Страница')
+    return HttpResponse('Страница приложения women')
+
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Страница по категориям</h1><p>id: {cat_id} </p>') 
@@ -12,6 +14,13 @@ def categories(request, cat_id):
 def categories_by_slug(request, cat_slug):
     return HttpResponse(f'<h1>Страница по категориям</h1><p>slug: {cat_slug} </p>') 
 
+
 def archive(request, year):
+    if year > 2023:
+        uri = reverse('cats', args=('music',))
+        return redirect(uri)
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year} </p>') 
-   
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
