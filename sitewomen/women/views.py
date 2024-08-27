586a3d81bc4362af4458fb98328418e1a57b3d13
Dmitry <dmitry1298@yandex.ru>
2024-08-27 14:52:27 +1000

    Add circle for in index.html

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,13 +4,23 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 
 
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+
+data_db = [ {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+            {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+            {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True}, ]
+
 
 def index(request):
-    return render(request, 'women/index.html')
+    return render(request, 'women/index.html', {'title':'Главная страница',
+                                                 'menu': menu,
+                                                 'posts': data_db})
 
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title':
+                                                 'О сайте'})
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Страница по категориям</h1><p>id: {cat_id} </p>') 
