521c782f7d862d4c4f1cdf67145b2ca336890366
Dmitry <dmitry1298@yandex.ru>
2024-10-12 20:26:09 +1000

    improve forms

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -31,6 +31,11 @@ def addpage(request):
         form = AddPostForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
     else:
         form = AddPostForm()
     data = {
