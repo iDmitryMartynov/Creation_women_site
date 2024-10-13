177d4e72968ee62972cab0048c49746380ac4dad
Dmitry <dmitry1298@yandex.ru>
2024-10-13 10:56:04 +1000

    create validators

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -1,16 +1,45 @@
+from typing import Any
 from django import forms
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.utils.deconstruct import deconstructible
+from django.core.exceptions import ValidationError
 from .models import Category, Husband
 
+
+@deconstructible
+class RussianValidator:
+    ALLOWED_CHARS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя0123456789- '
+    code = 'russian'
+
+    def __init__(self, message=None) -> None:
+        self.message = message if message else 'Должны присутствовать только русские символы дефис и пробел'
+
+    def __call__(self, value, *args: Any, **kwds: Any) -> Any:
+        if not(set(value) <= set(self.ALLOWED_CHARS)):
+            raise ValidationError(self.message, code=self.code)
+
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    slug = forms.SlugField(max_length=255, label='URL')
+    title = forms.CharField(max_length=255, label='Заголовок',
+                            # validators=[
+                            #     RussianValidator()
+                            # ],
+                            widget=forms.TextInput(attrs={'class': 'form-input'}), error_messages={
+                            'min_length': 'Слишком короткий заголовок',
+                            'required': 'Без заголовка никак'
+                            })
+    slug = forms.SlugField(max_length=255, label='URL',
+                            validators=[
+                                MinLengthValidator(5, message='Минимум 5 символов'),
+                                MaxLengthValidator(100, message='Максимум 100 символов')
+                            ])
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Контент')
     is_published = forms.BooleanField(required=False, label='Статус', initial=True)
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категории', empty_label='Категория не выбрана')
     husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, label='Муж', empty_label="Не замужем")
-    # time_create = forms.
-    # time_update =
     
-    
-    # tags =
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        ALLOWED_CHARS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя0123456789- '
+        if not(set(title) <= set(ALLOWED_CHARS)):
+            raise ValidationError('Должны присутствовать только русские символы дефис и пробел')
     
--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,4 +1,5 @@
 from tabnanny import verbose
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
@@ -24,7 +25,10 @@ class Women(models.Model):
         DRAFT= 0, 'Черновик'
         PUBLISHED = 1, 'Опубликованно'
     title = models.CharField(max_length=255, verbose_name='Заголовок')
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug', validators=[
+                                MinLengthValidator(5, message='Минимум 5 символов'),
+                                MaxLengthValidator(100, message='Максимум 100 символов')
+                            ])
     content = models.TextField(blank=True, verbose_name='Текст статьи')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
